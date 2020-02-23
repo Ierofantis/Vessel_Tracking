cb732b5e5a2b8094d5ddf05074673eab0d3ba09e
Theodore Pantazopoulos <teopanta1986@gmail.com>
2020-02-24 00:24:14 +0200

    reformat some code

--- app/Http/Middleware/LimiterPerIp.php
+++ app/Http/Middleware/LimiterPerIp.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
+
 use App\Models\UserIp;
 use Carbon\Carbon;
 
@@ -15,26 +16,24 @@ class LimiterPerIp
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request,Closure $next)
+    public function handle($request, Closure $next)
     {
         $requestUserIp = UserIp::where('ip', $request->ip())->get();
 
-        if(count($requestUserIp) === 0){
-           UserIp::create(['ip' => $request->ip()]);
-        }
-
-        else{
+        if (count($requestUserIp) === 0) {
+            UserIp::create(['ip' => $request->ip()]);
+        } else {
             try {
-               foreach ($requestUserIp as $item) {
-                if(Carbon::now()->format('h') - Carbon::parse($item['created_at'])->format('h') >= 10){
-                 \Log::error($item['ip'] . ' have exceeded the limit of requests.');
-                 return response('You have exceeded the limit of requests.', 401);
+                foreach ($requestUserIp as $item) {
+                    if (Carbon::now()->format('h') - Carbon::parse($item['created_at'])->format('h') >= 10) {
+                        \Log::error($item['ip'] . ' have exceeded the limit of requests.');
+                        return response('You have exceeded the limit of requests.', 401);
+                    }
                 }
-             }
             } catch (\Exception $e) {
                 \Log::error($e->getMessage());
             }
         }
         return $next($request);
-  }
+    }
 }
--- tests/TestQueries.php
+++ tests/TestQueries.php
@@ -1,101 +1,98 @@
 <?php
 
-use Laravel\Lumen\Testing\DatabaseMigrations;
-use Laravel\Lumen\Testing\DatabaseTransactions;
 use GuzzleHttp\Client as GuzzleClient;
 
 class TestQueries extends TestCase
 {
-   
-        protected $client;
 
-        protected function setUp(): void
-        {
-            $this->client = new GuzzleClient([
-                'base_uri' => 'http://localhost:8000'
-            ]);
-        }
+    protected $client;
 
-        public function test_mmsi_generic_test()
-        {
-            $response = $this->client->get('/vessels', [
-                'query' => [
-                    'mmsi' => 247039300
-                ]
-            ]);
-    
-            $this->assertEquals(200, $response->getStatusCode());
-            $body = json_decode($response->getBody());
-             if (isset($body, $body->data) && count($body->data) > 0) {
-                $data = $body->data;
-             foreach ($data as $d){   
+    protected function setUp(): void
+    {
+        $this->client = new GuzzleClient([
+            'base_uri' => 'http://localhost:8000'
+        ]);
+    }
+
+    public function test_mmsi_generic_test()
+    {
+        $response = $this->client->get('/vessels', [
+            'query' => [
+                'mmsi' => 247039300
+            ]
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $body = json_decode($response->getBody());
+        if (isset($body, $body->data) && count($body->data) > 0) {
+            $data = $body->data;
+            foreach ($data as $d) {
                 $this->assertEquals(247039300, $d->mmsi);
-               }
-            }            
-        }
-
-        public function test_timestamp()
-        {
-            $response = $this->client->get('/vessels', [
-                'query' => [
-                    'timestamp' => 1372700100
-                ]
-            ]);
-    
-            $this->assertEquals(200, $response->getStatusCode());
-            $body = json_decode($response->getBody());
-             if (isset($body, $body->data) && count($body->data) > 0) {
-                $data = $body->data;
-             foreach ($data as $d){   
-                $this->assertEquals(1372700100, $d->timestamp);
-               }
-            }            
-        }
-
-        public function test_mmsi_multiple_queries_test()
-        {
-            $response = $this->client->get('/vessels', [
-                'query' => [
-                    'mmsi' => '311486000', '311040700'
-                ]
-            ]);
-            
-            $this->assertEquals(200, $response->getStatusCode());
-            $body = json_decode($response->getBody());
-             if (isset($body, $body->data) && count($body->data) > 0) {
-                $data = $body->data;
-             foreach ($data as $d){    
-                 $this->assertContains(311486000, (array)$d);
-                 //Ideally I would like this part of code too but i couldnt find why the comma separated values on query param
-                 //from guzzlie client malformed my request
-                 //$this->assertContains(311040700, (array)$d);
-               }
-            }            
-        }
-      
-        public function test_lat_lon()
-        {
-            $response = $this->client->get('/vessels', [
-                'query' => [
-                    'minLat'=> 42.75178,
-                    'maxLat'=> 43.81345,
-                    'minLon'=> 15.4415,
-                    'maxLon'=> 16.21578
-                ]
-            ]);
-    
-            $this->assertEquals(200, $response->getStatusCode());
-    
-            $body = json_decode($response->getBody());
-
-             if (isset($body, $body->data) && count($body->data) > 0) {
-                $data = $body->data;
-
-                foreach ($data as $d){               
-                    $this->assertNotTrue($d->lat > 43.81345);
-                    $this->assertNotTrue($d->lon > 16.21578);
-                }
-            }            
+            }
         }
     }
 
+    public function test_timestamp()
+    {
+        $response = $this->client->get('/vessels', [
+            'query' => [
+                'timestamp' => 1372700100
+            ]
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $body = json_decode($response->getBody());
+        if (isset($body, $body->data) && count($body->data) > 0) {
+            $data = $body->data;
+            foreach ($data as $d) {
+                $this->assertEquals(1372700100, $d->timestamp);
+            }
+        }
+    }
+
+    public function test_mmsi_multiple_queries_test()
+    {
+        $response = $this->client->get('/vessels', [
+            'query' => [
+                'mmsi' => '311486000', '311040700'
+            ]
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $body = json_decode($response->getBody());
+        if (isset($body, $body->data) && count($body->data) > 0) {
+            $data = $body->data;
+            foreach ($data as $d) {
+                $this->assertContains(311486000, (array) $d);
+                //Ideally I would like this part of code too but i couldnt find why the comma separated values on query param
+                //from guzzlie client malformed my request
+                //$this->assertContains(311040700, (array)$d);
+            }
+        }
+    }
+
+    public function test_lat_lon()
+    {
+        $response = $this->client->get('/vessels', [
+            'query' => [
+                'minLat' => 42.75178,
+                'maxLat' => 43.81345,
+                'minLon' => 15.4415,
+                'maxLon' => 16.21578
+            ]
+        ]);
+
+        $this->assertEquals(200, $response->getStatusCode());
+
+        $body = json_decode($response->getBody());
+
+        if (isset($body, $body->data) && count($body->data) > 0) {
+            $data = $body->data;
+
+            foreach ($data as $d) {
+                $this->assertNotTrue($d->lat > 43.81345);
+                $this->assertNotTrue($d->lon > 16.21578);
+            }
+        }
+    }
+}
